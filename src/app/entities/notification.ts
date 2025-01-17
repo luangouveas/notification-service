186d8250bb1cb0318e111cd849fe98cb75de333b
Luan Gouveas <lgouveas@rhmed.com.br>
2025-01-17 17:08:57 -0300

    refact: Entidade de notificação gerando ID

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,5 +1,6 @@
 import { Replace } from 'src/helpers/Replace';
 import { Content } from './content';
+import { randomUUID } from 'node:crypto';
 
 export interface NotificationProps {
   recipientId: string;
@@ -10,10 +11,16 @@ export interface NotificationProps {
 }
 
 export class Notification {
+  private _id: string;
   private props: NotificationProps;
 
   constructor(props: Replace<NotificationProps, { createAt?: Date }>) {
+    this._id = randomUUID();
     this.props = { ...props, createAt: props.createAt ?? new Date() };
+  }
+
+  public get id(): string {
+    return this._id;
   }
 
   public get recipientId(): string {
