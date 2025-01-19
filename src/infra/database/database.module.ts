e9f9f6cd24e4215b4f12127c5a04505f02e7bce4
Luan Gouveas <lgouveas@rhmed.com.br>
2025-01-19 14:38:50 -0300

    refact: Ajustando imports com paths

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
-import { NotificationsRepository } from '../../app/repositories/notifications-repository';
+import { NotificationsRepository } from '@app/repositories/notifications-repository';
 import { PrismaNotificationRepository } from './prisma/repositories/prisma-notification-repository';
 
 @Module({
