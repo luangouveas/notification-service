81f943181bd485bd7c82faae79884e86b850127e
Luan Gouveas <lgouveas@rhmed.com.br>
2025-01-30 12:19:42 -0300

    Criando caso de uso para buscar notificações por recipiente

---
+++ src/app/use-cases/get-recipient-notifications.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+import { Notification } from '@app/entities/notification';
+
+interface GetRecipientNotificationRequest {
+  recipientId: string;
+}
+
+interface GetRecipientNotificationResponse {
+  notifications: Notification[];
+}
+
+@Injectable()
+export class GetRecipientNotifications {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(
+    request: GetRecipientNotificationRequest,
+  ): Promise<GetRecipientNotificationResponse> {
+    const { recipientId } = request;
+
+    const notifications =
+      await this.notificationRepository.findManyByRecipientId(recipientId);
+
+    return {
+      notifications,
+    };
+  }
+}
