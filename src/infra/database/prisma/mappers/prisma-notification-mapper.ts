f8b14dfc4a8333558a1be7ef137e3066d50532c9
Luan Gouveas <lgouveas@rhmed.com.br>
2025-01-30 14:48:42 -0300

    feat: Criando mapper para conversão da entidade de notificação do prisma para entidade de dominio

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as RawNotification } from '@prisma/client';
 import { Notification } from '@app/entities/notification';
+import { Content } from '@app/entities/content';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -12,5 +14,19 @@ export class PrismaNotificationMapper {
         createAt: notification.createAt,
       },
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createAt: raw.createAt,
+      },
+      raw.id,
+    );
   }
 }
